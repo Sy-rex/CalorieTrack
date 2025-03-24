258f2d8b6f4ac773611074958218935107a25d11
Kirill <sobolevkirill0@gmail.com>
2025-03-24 13:54:48 +0300

    Initial commit

---
+++ CalorieTrack/src/main/java/com/sobolev/spring/calorietrack/CalorieTrackApplication.java
@@ -0,0 +1,13 @@
+package com.sobolev.spring.calorietrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalorieTrackApplication.class, args);
+    }
+
+}
